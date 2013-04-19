efbd933813a6d273962a3957e17fdf4fb40f5b04
Jan Breuer <jan.breuer@jaybee.cz>
2013-04-19 11:20:17 +0200

    Resolve bug with short commands

--- libscpi/src/utils.c
+++ libscpi/src/utils.c
@@ -42,6 +42,8 @@
 #include "utils.h"
 
 static size_t patternSeparatorShortPos(const char * pattern, size_t len);
+static size_t patternSeparatorPos(const char * pattern, size_t len);
+static size_t cmdSeparatorPos(const char * cmd, size_t len);
 
 /**
  * Find the first occurrence in str of a character in set.
@@ -372,6 +374,35 @@ size_t skipWhitespace(const char * cmd, size_t len) {
     return len;
 }
 
+/** 
+ * is colon or not.  add by hmm 2013.4.1
+ * @param cmd - command
+ * @return
+ */
+static bool_t iscolon(const char * cmd) {
+    char* pColon = ":";
+    if(0 == SCPI_strncasecmp(cmd, pColon, 1))
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+/**
+ * Count colon from the beggining  add by hmm 2013.4.1
+ * @param cmd - command
+ * @param len - max search length
+ * @return number of colon
+ */
+size_t skipColon(const char * cmd, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++) {
+        if (!iscolon(&cmd[i])) {
+            return i;
+        }
+    }
+    return len;
+}
 
 /**
  * Pattern is composed from upper case an lower case letters. This function
@@ -391,6 +422,41 @@ size_t patternSeparatorShortPos(const char * pattern, size_t len) {
 }
 
 /**
+ * Find pattern separator position
+ * @param pattern
+ * @param len - max search length
+ * @return position of separator or len
+ */
+size_t patternSeparatorPos(const char * pattern, size_t len) {
+    
+    char * separator = strnpbrk(pattern, len, "?:[]");
+    if (separator == NULL) {
+        return len;
+    } else {
+        return separator - pattern;
+    }
+}
+
+/**
+ * Find command separator position
+ * @param cmd - input command
+ * @param len - max search length
+ * @return position of separator or len
+ */
+size_t cmdSeparatorPos(const char * cmd, size_t len) {
+    char * separator = strnpbrk(cmd, len, ":?");
+    size_t result;
+    if (separator == NULL) {
+        result = len;
+    } else {
+        result = separator - cmd;
+    }
+    
+    return result;
+}
+
+
+/**
  * Match pattern and str. Pattern is in format UPPERCASElowercase
  * @param pattern
  * @param pattern_len
@@ -402,6 +468,67 @@ bool_t matchPattern(const char * pattern, size_t pattern_len, const char * str, 
     int pattern_sep_pos_short = patternSeparatorShortPos(pattern, pattern_len);
     return compareStr(pattern, pattern_len, str, str_len) ||
             compareStr(pattern, pattern_sep_pos_short, str, str_len);
+}
+
+/**
+ * Compare pattern and command
+ * @param pattern
+ * @param cmd - command
+ * @param len - max search length
+ * @return TRUE if pattern matches, FALSE otherwise
+ */
+bool_t matchCommand(const char * pattern, const char * cmd, size_t len) {
+    int result = FALSE;
+    
+    const char * pattern_ptr = pattern;
+    int pattern_len = strlen(pattern);
+    const char * pattern_end = pattern + pattern_len;
+    
+    const char * cmd_ptr = cmd;
+    size_t cmd_len = SCPI_strnlen(cmd, len);
+    const char * cmd_end = cmd + cmd_len;
+    
+    while (1) {
+        int pattern_sep_pos = patternSeparatorPos(pattern_ptr, pattern_end - pattern_ptr);
+        int cmd_sep_pos = cmdSeparatorPos(cmd_ptr, cmd_end - cmd_ptr);
+        
+        if (matchPattern(pattern_ptr, pattern_sep_pos, cmd_ptr, cmd_sep_pos)) {
+            pattern_ptr = pattern_ptr + pattern_sep_pos;
+            cmd_ptr = cmd_ptr + cmd_sep_pos;
+            result = TRUE;
+            
+            /* command is complete */
+            if ((pattern_ptr == pattern_end) && (cmd_ptr >= cmd_end)) {
+                break;
+            }
+            
+            /* pattern complete, but command not */
+            if ((pattern_ptr == pattern_end) && (cmd_ptr < cmd_end)) {
+                result = FALSE;
+                break;
+            }
+            
+            /* command complete, but pattern not */
+            if (cmd_ptr >= cmd_end) {
+                result = FALSE;
+                break;
+            }
+            
+            /* both command and patter contains command separator at this position */
+            if ((pattern_ptr[0] == cmd_ptr[0]) && ((pattern_ptr[0] == ':') || (pattern_ptr[0] == '?'))) {
+                pattern_ptr = pattern_ptr + 1;
+                cmd_ptr = cmd_ptr + 1;
+            } else {
+                result = FALSE;
+                break;
+            }
+        } else {
+            result = FALSE;
+            break;
+        }
+    }
+    
+    return result;
 }
 
 
